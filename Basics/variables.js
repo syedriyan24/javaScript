e2f757ae3848d151717912f2f6d50cd92f9326dc
syedriyan24 <syedriyan7628@gmail.com>
2025-02-03 20:17:11 +0530

    variables

---
+++ Basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 202501
+let accountEmail = "syed@google.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+
+//const value not change
+//accountId = 2
+
+accountEmail = "ryan@gmail.com"
+accountPassword= "221212"
+accountCity = "mysore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// do not use var because block scope and function scope
+
